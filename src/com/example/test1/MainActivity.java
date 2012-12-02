86dd574ba71156e0d156a839dc3badbaecc3041e
Ludovic <ludovic.girard@etudiant.univ-rennes1.fr>
2012-12-02 14:34:37 +0100

    a

--- src/com/example/test1/MainActivity.java
+++ src/com/example/test1/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.test1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+
 public class MainActivity extends Activity {
-	private TextView coucou = null;
+	private TextView text1 = null;
+	
 	
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			coucou = new TextView(this);
-			coucou.setText("Salut c'est Pinpin !");
-			setContentView(coucou);
+			setContentView(R.layout.activity_main);
+			
+			text1 = (TextView)findViewById(R.id.text);
+			text1.setPadding(250, 60, 10, 90);
+			
+			setContentView(R.layout.activity_main);			
 			}
 }
