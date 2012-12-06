a6eabd7aa9a6241ee07e8164deec7ece30497df3
Ludovic <ludovic.girard@etudiant.univ-rennes1.fr>
2012-12-06 13:26:15 +0100

    e

--- src/com/example/test1/MainActivity.java
+++ src/com/example/test1/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.test1;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private TextView text1 = null;
 	
 	
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_main);
+			// Création du listener pour le bouton "valider" et récupération et attachement
+	        // de ce listener au bouton.
+	        OnClickListener connexionListener = new OnClickListener() {
 			
+	            private Intent page_lumiere;
+
+				/** Action effectuée lors du clic sur le bouton Valider
+                 *  @param actualView - Vue dans laquelle on se trouve
+                 */
+	            {
+	            	
+	            			
+	            }
+                public void onClick(View activity_main) {
+                                               
+                     
+                        startActivity(page_lumiere);
+                        setContentView(R.layout.page_lumiere);
+                        //startActivity(intent);
+                }
 			
-			setContentView(R.layout.activity_main);			
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					
+				}
+	        }	;	
 			}
 }
